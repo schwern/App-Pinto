be860349d3326d6cd973909d1eaf4da63600c085
Jeffrey Ryan Thalhammer <jeff@imaginative-software.com>
2012-05-08 13:22:35 -0700

    Options for controlling local-lib with cpanm.

--- lib/App/Pinto/Command/install.pm
+++ lib/App/Pinto/Command/install.pm
@@ -19,11 +19,29 @@ sub opt_spec {
     my ($self, $app) = @_;
 
     return (
-        [ 'cpanm_exe|cpanm=s'  => 'Path to the cpanm executable'      ],
-        [ 'cpanm_options|o:s%' => 'name=value pairs of cpanm options' ],
-        [ 'stack|s=s'          => 'Use the index for this stack'      ],
+        [ 'cpanm-exe|cpanm=s'       => 'Path to the cpanm executable'                 ],
+        [ 'cpanm-options|o:s%'      => 'name=value pairs of cpanm options'            ],
+        [ 'local-lib|l=s'           => 'install into a local lib directory'           ],
+        [ 'local-lib-contained|L=s' => 'install into a contained local lib directory' ],
+        [ 'stack|s=s'               => 'Use the index for this stack'                 ],
 
     );
+}
+
+#------------------------------------------------------------------------------
+
+sub validate_args {
+    my ($self, $opts, $args) = @_;
+
+    my $local_lib = delete $opts->{local_lib};
+    $opts->{cpanm_options}->{'local-lib'} = $local_lib
+        if $local_lib;
+
+    my $local_lib_contained = delete $opts->{local_lib_contained};
+    $opts->{cpanm_options}->{'local-lib-contained'} = $local_lib_contained
+        if $local_lib_contained;
+
+    return 1;
 }
 
 #------------------------------------------------------------------------------
@@ -85,15 +103,15 @@ or ';') will be ignored.
 
 =over 4
 
-=item --cpanm_exe=PATH
+=item --cpanm-exe PATH
 
-=item --cpanm=PATH
+=item --cpanm PATH
 
 Sets the path to the L<cpanm> executable.  If not specified, the
 C<PATH> will be searched for the executable.  At present, cpanm
 version 1.500 or newer is required.
 
-=item --cpanm_options NAME=VALUE
+=item --cpanm-options NAME=VALUE
 
 =item -o NAME=VALUE
 
@@ -101,6 +119,21 @@ These are options that you wish to pass to L<cpanm>.  Do not prefix
 the option name with a '-'.  You can pass any option you like, but the
 C<--mirror> and C<--mirror-only> options will always be set to point
 to the Pinto repository.
+
+=item --local-lib DIRECTORY
+
+=item -l DIRECTORY
+
+Shortcut for setting the C<--local-lib> option on L<cpanm>.  Same as
+C<--cpanm-options local-lib=DIRECTORY> or C<-o l=DIRECTORY>.
+
+=item --local-lib-contained DIRECTORY
+
+=item -l DIRECTORY
+
+Shortcut for setting the C<--local-lib-contained> option on L<cpanm>.
+Same as C<--cpanm-options local-lib-containted=DIRECTORY> or C<-o
+L=DIRECTORY>.
 
 =item --stack=NAME
 
