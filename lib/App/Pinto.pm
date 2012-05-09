b1c21aac42f0e95fe0c8007e1f60b86453874965
Jeffrey Ryan Thalhammer <jeff@imaginative-software.com>
2012-05-09 13:50:38 -0700

    POD Edits

--- lib/App/Pinto.pm
+++ lib/App/Pinto.pm
@@ -115,13 +115,21 @@ __END__
 
 =head1 DESCRIPTION
 
-App::Pinto is the command-line drive for Pinto.  It is just a
+App::Pinto is the command-line driver for Pinto.  It is just a
 front-end.  To do anything useful, you'll also need to install one of
 the back-ends, which ship separately.  If you need to create
-repositories and work directly with repositories on the local disk,
+repositories and/or work directly with repositories on the local disk,
 then install L<Pinto>.  If you already have a repository on a remote
 host that is running L<pintod>, then install L<Pinto::Remote>.  If
-you're not sure what you need then install L<Task::Pinto> to get the
-whole wad.
+you're not sure what you need, then install L<Task::Pinto> to get the
+whole kit.
+
+=head1 SEE ALSO
+
+L<Pinto::Manual> for general information on using Pinto.
+
+L<pinto> to create and manage a Pinto repository.
+
+L<pintod> to allow remote access to your Pinto repository.
 
 =cut
