6f5eba1fb48fae51c00bb72fdce5c63f2e5f1909
Jeffrey Ryan Thalhammer <jeff@imaginative-software.com>
2012-05-09 13:44:58 -0700

    POD edits.

--- lib/App/Pinto.pm
+++ lib/App/Pinto.pm
@@ -112,3 +112,16 @@ sub default_log_colors { return $PINTO_DEFAULT_LOG_COLORS }
 1;
 
 __END__
+
+=head1 DESCRIPTION
+
+App::Pinto is the command-line drive for Pinto.  It is just a
+front-end.  To do anything useful, you'll also need to install one of
+the back-ends, which ship separately.  If you need to create
+repositories and work directly with repositories on the local disk,
+then install L<Pinto>.  If you already have a repository on a remote
+host that is running L<pintod>, then install L<Pinto::Remote>.  If
+you're not sure what you need then install L<Task::Pinto> to get the
+whole wad.
+
+=cut
