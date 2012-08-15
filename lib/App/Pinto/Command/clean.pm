598a4ceb53195e29b2915258e73e8ef7413a5240
Jeffrey Ryan Thalhammer <jeff@imaginative-software.com>
2012-08-15 16:59:21 -0700

    New clean command.

---
+++ lib/App/Pinto/Command/clean.pm
@@ -0,0 +1,45 @@
+# ABSTRACT: remove orphaned distribution archives
+
+package App::Pinto::Command::clean;
+
+use strict;
+use warnings;
+
+#------------------------------------------------------------------------------
+
+use base 'App::Pinto::Command';
+
+#------------------------------------------------------------------------------
+
+# VERSION
+
+#-----------------------------------------------------------------------------
+
+1;
+
+__END__
+
+=head1 SYNOPSIS
+
+  pinto --root=REPOSITORY_ROOT clean
+
+=head1 DESCRIPTION
+
+The database for L<Pinto> is transactional, so failures and aborted
+commands do not change the indexes.  However, the filesystem where
+distribution archives are physically stored is not transactional and
+may become cluttered with archives that are not on any stack.
+
+Normally, L<Pinto> tries to clean up those orphaned archives.  But in
+some cases it might not.  Running this command will force their
+removal.
+
+=head1 COMMAND ARGUMENTS
+
+None.
+
+=head1 COMMAND OPTIONS
+
+None.
+
+=cut
