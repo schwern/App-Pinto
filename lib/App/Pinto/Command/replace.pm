589d1dbf833c9f35ed5f1372ffa7e0588f721812
Jeffrey Ryan Thalhammer <jeff@imaginative-software.com>
2012-09-23 09:59:44 -0700

    Revise POD.

--- lib/App/Pinto/Command/replace.pm
+++ lib/App/Pinto/Command/replace.pm
@@ -32,7 +32,7 @@ sub opt_spec {
 sub validate_args {
     my ($self, $opts, $args) = @_;
 
-    $self->usage_error('Must specify OLD_DIST_SPEC and NEW_ARCHIVE')
+    $self->usage_error('Must specify ARCHIVE_FILE and TARGET')
         if @{$args} != 2;
 
     return 1;
