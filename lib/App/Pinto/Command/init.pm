5cb4f274b105da8b1a771a08b17a1f45ead75f87
Jeffrey Ryan Thalhammer <jeff@imaginative-software.com>
2012-12-02 02:40:49 -0800

    Change the way the initial stack is given at command line.
    We're no longer assuming that the repository has an initial stack,
    or that the initial stack is the default.  You can now control all
    of that at the command line.

--- lib/App/Pinto/Command/init.pm
+++ lib/App/Pinto/Command/init.pm
@@ -21,11 +21,10 @@ sub opt_spec {
     my ($self, $app) = @_;
 
     return (
-        [ 'bare'          => 'Do not create any initial stack'         ],
-        [ 'description=s' => 'Description of the initial stack'        ],
-        [ 'log_level=s'   => 'Minimum logging level for the log file'  ],
-        [ 'source=s@'     => 'URL of upstream repository (repeatable)' ],
-        [ 'stack|s=s'     => 'Name of the initial stack'               ],
+        [ 'description=s' => 'Description of the initial stack'             ],
+        [ 'nodefault'     => 'Do not mark the initial stack as the default' ],
+        [ 'log-level=s'   => 'Minimum logging level for the log file'       ],
+        [ 'source=s@'     => 'URL of upstream repository (repeatable)'      ],
     );
 }
 
@@ -34,11 +33,14 @@ sub opt_spec {
 sub validate_args {
     my ($self, $opts, $args) = @_;
 
-    $self->usage_error('Arguments are not allowed')
-      if @{ $args };
+    $self->usage_error('Only one stack argument is allowed')
+      if @{ $args } > 1;
 
-    $self->usage_error('Cannot use --bare with --stack or --description')
-      if $opts->{bare} and ($opts->{stack} or $opts->{description});
+    $self->usage_error('Cannot use --description without specifying a stack')
+      if $opts->{description} and not @{ $args };
+
+    $self->usage_error('Cannot use --nodefault without specifying a stack')
+      if $opts->{nodefault} and not @{ $args };
 
     return 1;
 }
@@ -63,6 +65,9 @@ sub execute {
     # TODO: Use a config file format that allows multiple values per key (MVP perhaps?).
     $opts->{sources} = join ' ', @{ delete $opts->{source} } if defined $opts->{source};
 
+    # Stuff the stack argument into the options hash (if it exists)
+    $opts->{stack} = $args->[0] if $args->[0];
+
     my $initializer = Pinto::Initializer->new( %{ $global_opts } );
     $initializer->init( %{$opts} );
     return 0;
@@ -78,37 +83,67 @@ __END__
 
 =head1 SYNOPSIS
 
-  pinto --root=REPOSITORY_ROOT init [OPTIONS]
+  pinto --root=REPOSITORY_ROOT init [OPTIONS] [STACK]
 
 =head1 DESCRIPTION
 
 This command creates a new, empty repository.  If the target directory
 does not exist, it will be created for you.  If it does already exist,
-then it must be empty.  The new repository will contain an empty (but
-valid) index file.  You can set the configuration parameters of the
-new repository using the command line options listed below.
+then it must be empty.  You can set the configuration properties of
+the new repository using the command line options listed below.
 
 =head1 COMMAND ARGUMENTS
 
-None.
+The argument is the name of the initial stack.  If given, the stack
+will be created and marked as the default stack (unless the
+C<--nodefault> option is specified).  Stack names must be alphanumeric
+plus hyphens and undercores, and are not case-sensitive.
+
+If the argument is not given, then no stack will be created.  But to
+do anything useful, you'll need to use the
+L<new|App::Pinto::Command::new> command to create a stack once the
+repository has been created.
+
+You should also consider marking a default stack by using the
+L<default|App::Pinto::Command::default> command.  Otherwise, you'll
+have to specify the C<--stack> option for most other commands.
 
 =head1 COMMAND OPTIONS
 
 =over 4
 
-=item --bare
-
-Do not create an initial stack in the repository.  Use this option
-if you plan to load the repository from a dump file.
-
 =item --description=TEXT
 
 A brief description of the initial stack.  Defaults to "the initial
-stack".
+stack".  This option is only allowed if the C<STACK> argument is
+given.
+
+
+=item --log-level=LEVEL
+
+Sets the minimum level for the repository log file.  Valid C<LEVEL>s
+are C<debug>, C<info>, C<notice>, C<warning>, or C<error>.  The
+default is C<notice>.
+
+Beware that lowering the log level may seriously degrade performance.
+You can change this property at any time by editing the repository
+configuration file at F<REPOSITORY_ROOT/.pinto/config/pinto.ini>.
+
+
+=item --nodefault
+
+Do not mark the initial stack as the default stack.  This option
+is only allowed if the C<STACK> argument is given.
+
+If you choose not to mark the default stack, then you'll be required
+to specify the C<--stack> option for most commands.  You can always
+mark (or ummark) the default stack by at any time by using the
+L<default|App::Pinto::Command::default> command.
+
 
 =item --source=URL
 
-The URL of a repository where foreign distributions will be pulled
+The URL of the upstream repository where distributions will be pulled
 from.  This is usually the URL of a CPAN mirror, and it defaults to
 L<http://cpan.perl.org>.  But it could also be a L<CPAN::Mini> mirror,
 or another L<Pinto> repository.
@@ -116,15 +151,7 @@ or another L<Pinto> repository.
 You can specify multiple repository URLs by repeating the C<--source>
 option.  Repositories that appear earlier in the list have priority
 over those that appear later.  See L<Pinto::Manual> for more
-information about using multiple source repositories.
-
-=item --stack=NAME
-
-=item -s NAME
-
-The name of the inital stack.  Stack names must be alphanumeric
-plus hyphens and undercores, and are not case sensitive.  Defalts
-to 'init'.
+information about using multiple upstream repositories.
 
 =back
 
