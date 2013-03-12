798a4375cf0ab3ab3751c8ad4c6dacb523c5794a
Jeffrey Ryan Thalhammer <jeff@imaginative-software.com>
2013-03-12 14:38:39 -0700

    Simplify command by merging arguments during validation.

--- lib/App/Pinto/Command/rename.pm
+++ lib/App/Pinto/Command/rename.pm
@@ -25,18 +25,10 @@ sub validate_args {
     $self->usage_error('Must specify FROM_STACK and TO_STACK')
         if @{$args} != 2;
 
+    $opts->{from_stack} = $args->[0];
+    $opts->{to_stack}   = $args->[1];
+
     return 1;
-}
-
-#------------------------------------------------------------------------------
-
-sub execute {
-    my ($self, $opts, $args) = @_;
-
-    my %stacks = ( from_stack => $args->[0], to_stack => $args->[1] );
-    my $result = $self->pinto->run($self->action_name, %{$opts}, %stacks);
-
-    return $result->exit_status;
 }
 
 #------------------------------------------------------------------------------
@@ -54,15 +46,12 @@ __END__
 
 This command changes the name of an existing stack.  Once the name is
 changed, you will not be able to perform commands or access archives
-via the old stack name.  Name changes are not recorded in the revision
-history, so the L<revert|App::Pinto::Command::revert> command will
-never cause the stack name to change back to its former value.
+via the old stack name.
 
-Please see the L<new|App::Pinto::Command::new> command to create a new
-empty stack, or the L<edit|App::Pinto::Command::edit> command to
-change a stack's properties after it has been created, or the
-L<copy|App::Pinto::Command::copy> command to duplicate an existing
-stack.
+See the L<new|App::Pinto::Command::new> command to create a new empty
+stack, or the L<copy|App::Pinto::Command::copy> command to duplicate
+an existing stack, or the L<props|App::Pinto::Command::props> command
+to change a stack's properties after it has been created.
 
 =head1 COMMAND ARGUMENTS
 
